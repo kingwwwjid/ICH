516e622120af2486b6008ed5d71a8ee01dc73608
禾穗 <2330666394@qq.com>
2026-09-16 15:44:03 +0800

    放置一些图片

---
+++ utils/data.js
@@ -0,0 +1,43 @@
+module.exports = {
+  banners: [
+    { id: 1, image: '/images/1.png', title: 'banner1' },
+  { id: 2, image: '/images/2.png', title: 'banner2' },
+  { id: 3, image: '/images/3.png', title: 'banner3' },
+  { id: 4, image: '/images/4.png', title: 'banner4' }
+  ],
+  activities: [
+    {
+      id: 1,
+      cover: '/images/1.png',
+      title: '浏阳烟花观赏',
+      time: '2026-09-20 14:00',
+      location: '浏阳',
+      detail: '欣赏烟花。'
+    },
+     {
+      id: 2,
+      cover: '/images/2.png',
+      title: '剪纸观赏',
+      time: '2026-09-22 14:00',
+      location: '湘潭',
+      detail: '欣赏剪纸艺术。'
+    },
+     {
+      id: 3,
+      cover: '/images/3.png',
+      title: '湘绣技艺体验',
+      time: '2026-09-24 14:00',
+      location: '长沙 ',
+      detail: '体验湘绣。'
+    },
+     {
+      id: 4,
+      cover: '/images/4.png',
+      title: '巫家拳',
+      time: '2026-09-26 14:00',
+      location: '湘西',
+      detail: '欣赏巫家拳。'
+    },
+  ],
+
+}
