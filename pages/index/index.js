1a9bb667c816d5b8c4916ad90a8d4db2d97fc3af
禾穗 <2330666394@qq.com>
2026-09-17 09:04:06 +0800

    跳转链接测试

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,49 +1,19 @@
 // index.js
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+const data = require('../../utils/data.js')//数据导入
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
-    },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    banners: data.banners,
+    activities: data.activities,
+    navItems: [//轮播下处副标
+      { id: 1, icon: '/images/1.png', text: '非遗活动' ,url: '/pages/activity/activity'},
+      { id: 2, icon: '/images/2.png', text: '非遗展馆' ,url: '/pages/gallery/gallery' },
+      { id: 3, icon: '/images/3.png', text: '非遗大师' ,url: '/pages/master/master'},
+      { id: 4, icon: '/images/4.png', text: '非遗旅游' ,url: '/pages/travel/travel'},
+    ]
   },
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onChooseAvatar(e) {
-    const { avatarUrl } = e.detail
-    const { nickName } = this.data.userInfo
-    this.setData({
-      "userInfo.avatarUrl": avatarUrl,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
-  },
-  onInputChange(e) {
-    const nickName = e.detail.value
-    const { avatarUrl } = this.data.userInfo
-    this.setData({
-      "userInfo.nickName": nickName,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
+   clk(e) {
+    const url = e.currentTarget.dataset.url
+    wx.navigateTo({ url })
+  }
 })
