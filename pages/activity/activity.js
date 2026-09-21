77105a8c0c2a85ce476984f980c61edcbd59fb15
禾穗 <2330666394@qq.com>
2026-09-21 22:44:45 +0800

    活动细节补充

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -1,3 +1,12 @@
+const data = require('../../utils/data.js')
+
 Page({
-  data: {}
+  data: {
+    activities: data.activities
+  },
+
+  onActTap(e) {
+    const id = e.currentTarget.dataset.id
+    wx.navigateTo({ url: '/pages/detailact/detailact?id=' + id })//跳转页面
+  }
 })
--- pages/detailact/detailact.js
+++ pages/detailact/detailact.js
@@ -1,3 +1,13 @@
+const data = require('../../utils/data.js')
+
 Page({
-  data: {}
+  data: {
+    act: null   // 一开始是空，找到后再填进去
+  },
+
+  onLoad(options) {//加载时自动调用
+    const id = Number(options.id)   // 拿到跳转时传的 id
+    const act = data.activities.find(item => item.id === id)  // 找出对应的活动
+    this.setData({ act })
+  }
 })
