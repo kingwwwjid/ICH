2fe716899817d3180610550ed0d28eca02a10cdb
禾穗 <2330666394@qq.com>
2026-09-21 22:55:10 +0800

    大师界面

--- pages/master/master.js
+++ pages/master/master.js
@@ -1,3 +1,12 @@
+const data = require('../../utils/data.js')
+
 Page({
-  data: {}
+  data: {
+    masters: data.masters
+  },
+
+  onMstTap(e) {
+    const id = e.currentTarget.dataset.id
+    wx.navigateTo({ url: '/pages/detailmst/detailmst?id=' + id })
+  }
 })
--- utils/data.js
+++ utils/data.js
@@ -39,5 +39,35 @@ module.exports = {
       detail: '欣赏巫家拳。'
     },
   ],
+  masters:[
+    {
+      id:1,
+      image:'/images/1.png',
+      name:'张三',
+      titile:'非遗传承人某某某',
+      intro:'大师介绍。' 
+},
+    {
+      id:2,
+      image:'/images/2.png',
+      name:'李四',
+      titile:'非遗传承人某某某',
+      intro:'大师介绍。'    
+    },
+    {
+      id:3,
+      image:'/images/3.png',
+      name:'王五',
+      titile:'非遗传承人某某某',
+      intro:'大师介绍。'    
+    },
+    {
+      id:4,
+      image:'/images/4.png',
+      name:'赵六',
+      titile:'非遗传承人某某某',
+      intro:'大师介绍。'
+    }
+  ],
 
 }
